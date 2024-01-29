efb1141e7e10a2271c560d88ce90b097a37e9289
akifguldemir <akifguldemir@gmail.com>
2024-01-29 23:21:14 +0300

    login page

---
+++ screens/Login.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View, Text } from "react-native";
+
+function Login() {
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+    </View>
+  );
+}
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 8,
+  },
+});
