10c09568c7bb4ced2acb5e731d4709d6410b94ed
akifguldemir <akifguldemir@bontesoft.com>
2024-02-01 01:57:09 +0300

    AuthService

---
+++ services/AuthService.js
@@ -0,0 +1,10 @@
+import { BaseService } from "./BaseServices"
+
+class AuthService extends BaseService {
+
+    // getAllNews(){
+    //     return this.get(GET_CITIES)
+    // }
+}
+
+export default new AuthService()
