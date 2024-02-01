8d48efb0e7bc623e4063a20fc80d5e048357cfeb
akifguldemir <akifguldemir@bontesoft.com>
2024-02-02 00:03:42 +0300

    update

--- store/store.js
+++ store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import citiesReducer from './citiesSlice'
+import authReducer from './authSlice'
 
 export const store = configureStore({
   reducer: {
-    cities: citiesReducer
+    cities: citiesReducer,
+    auth: authReducer
   },
 })
