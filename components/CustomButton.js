0abd496122cde8bf689edd1062898b058a5a9a89
akifguldemir <akifguldemir@bontesoft.com>
2024-02-05 17:44:00 +0300

    ActivityIndicator added

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, ActivityIndicator } from 'react-native';
 
-const CustomButton = ({ title, onPress, style }) => {
+const CustomButton = ({ title, onPress, style, loading }) => {
   return (
     <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+      {loading && <ActivityIndicator size="small"/>}
       <Text style={styles.buttonText}>{title}</Text>
     </TouchableOpacity>
   );
@@ -17,8 +18,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginVertical: 8,
     borderRadius: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   buttonText: {
+    marginLeft: 4,
     color: '#fff',
     fontSize: 16,
   },
