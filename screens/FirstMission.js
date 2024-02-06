1f26bba09ea291743a6c2e295731649289cebb2e
akifguldemir <akifguldemir@bontesoft.com>
2024-02-07 02:08:39 +0300

    logout tested

--- screens/FirstMission.js
+++ screens/FirstMission.js
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/authSlice";
 
 function FirstMission() {
+  dispatch = useDispatch();
+
+  const logoutAuth = () => {
+    dispatch(logout);
+  };
+
+  return (
     <View>
-        <Text>First Mission</Text>
+      <Text>First Mission</Text>
+      <Button onPress={logoutAuth} title="Çıkış"></Button>
     </View>
+  );
 }
 
 export default FirstMission;
