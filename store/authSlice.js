9048c426d40987a034b233856f48ddeebdcb4cbb
akifguldemir <akifguldemir@bontesoft.com>
2024-02-07 02:09:06 +0300

    logout process

--- store/authSlice.js
+++ store/authSlice.js
@@ -25,6 +25,15 @@ export const authSlice = createSlice({
       // return AuthService.signUp(action.payload);
     },
     loginSuccess(state, action) {
+      state.isLoggedIn = true;
+      state.token = action.payload.token;
+      state.refreshToken = action.payload.refreshToken;
+      state.email = action.payload.email;
+      LocalStorage.setItem("token", action.payload.token);
+      LocalStorage.setItem("refreshToken", action.payload.refreshToken);
+      LocalStorage.setItem("email", action.payload.email);
+    },
+    setLogin(state, action) {
       state.isLoggedIn = true
       state.token = action.payload.token
       state.refreshToken = action.payload.refreshToken
@@ -35,46 +44,63 @@ export const authSlice = createSlice({
       state.isLoggedIn = false;
       state.token = null;
       state.refreshToken = null;
-      // localStorage.removeItem("user");
-      // localStorage.removeItem("token");
-      // localStorage.removeItem("refreshToken");
-      // localStorage.removeItem("userType");
     },
     setLoading(state, action) {
       state.isLoading = action.payload;
     },
   },
-  middleware:[]
+  middleware: [],
 });
 
-export const { checkIsLoggedInAuth, signUpAuth, loginSuccess, logoutAuth, setLoading } =
-  authSlice.actions;
+export const {
+  checkIsLoggedInAuth,
+  signUpAuth,
+  loginSuccess,
+  setLogin,
+  logoutAuth,
+  setLoading,
+} = authSlice.actions;
 
-export const logout = () => (dispatch) => {
+export const logout = () => async (dispatch) => {
+  await LocalStorage.removeItem("user");
+  await LocalStorage.removeItem("token");
+  await LocalStorage.removeItem("refreshToken");
   dispatch(logoutAuth);
 };
 
-export const getToken = () => (dispatch) => {
-};
+export const getToken = () => (dispatch) => {};
 
-export const renewToken = () => (dispatch) => {
-};
+export const renewToken = () => (dispatch) => {};
 
 export const signUp = (data) => (dispatch) => {};
 
-export const login = (data) => async (dispatch) => {
-  try {
-    dispatch(setLoading(true));
-    const response = await AuthService.login(data);
-    if (response.status === 200) {
-      response.data.email = data.email
-      dispatch(loginSuccess(response.data));
+export const login =
+  (data, isFirstLogin = true) =>
+  async (dispatch) => {
+    try {
+      if (isFirstLogin) {
+        dispatch(setLoading(true));
+
+        const response = await AuthService.login(data);
+        console.log(response)
+        if (response.status === 200) {
+          response.data.email = data.email;
+          dispatch(loginSuccess(response.data));
+        }
+      } else {
+        const data = {
+          token: await LocalStorage.getItem("token"),
+          refreshToken: await LocalStorage.getItem("refreshToken"),
+          email: await LocalStorage.getItem("email"),
+        };
+
+        dispatch(setLogin(data))
+      }
+    } catch (error) {
+      // Hata durumlarına göre işlemler
+    } finally {
+      dispatch(setLoading(false));
     }
-  } catch (error) {
-    // Hata durumlarına göre işlemler
-  } finally {
-    dispatch(setLoading(false));
-  }
-};
+  };
 
 export default authSlice.reducer;
